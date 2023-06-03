3bdfb4cc4f055ae56611c6d91ec807eb3b6c8f17
adilkenzh17 <skeezygod@gmail.com>
2023-06-04 00:15:53 +0600

    Testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WeightedGraph<String> graph = new WeightedGraph<>();  // testing;
+        Vertex<String> vertexA = new Vertex<>("A");
+        Vertex<String> vertexB = new Vertex<>("B");
+        Vertex<String> vertexC = new Vertex<>("C");
+        Vertex<String> vertexD = new Vertex<>("D");
+        Vertex<String> vertexE = new Vertex<>("E");
+
+        graph.addVertex(vertexA);
+        graph.addVertex(vertexB);
+        graph.addVertex(vertexC);
+        graph.addVertex(vertexD);
+        graph.addVertex(vertexE);
+
+        graph.addEdge(vertexA, vertexB, 5);
+        graph.addEdge(vertexA, vertexC, 3);
+        graph.addEdge(vertexB, vertexD, 2);
+        graph.addEdge(vertexC, vertexD, 1);
+        graph.addEdge(vertexD, vertexE, 4);
+
+        BFS<String> bfs = new BFS<>(graph, vertexA);
+        List<Vertex<String>> bfsPath = bfs.getPath(vertexE);
+        System.out.println("BFS Path: " + bfsPath);
+
+        DijkstraSearch<String> dijkstra = new DijkstraSearch<>(graph, vertexA);
+        List<Vertex<String>> dijkstraPath = dijkstra.getPath(vertexE);
+
+        System.out.println("Dijkstra path: " + dijkstraPath);
     }
 }
